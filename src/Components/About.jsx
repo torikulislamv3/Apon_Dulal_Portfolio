98d1054adca648e1941588a7dbd8466eda7d2e1f
MD TORIKUL ISLAM <tamim.officialinfo7@gmail.com>
2024-07-15 03:12:25 +0600

    aboute section added

---
+++ src/Components/About.jsx
@@ -0,0 +1,28 @@
+export const About = () => {
+  return (
+   <div>
+        <h1 className="text-4xl font-bold text-[#1877F2] text-center mt-5">
+            About APON DULAL
+        </h1>
+        <p className="text-center"><progress className="progress w-56"></progress></p>
+        <section>
+        <div className="hero">
+  <div className="hero-content flex-col items-center lg:flex-row-reverse">
+    <img
+      src="https://i.postimg.cc/m2JD9g3m/Apon-Dulal-removebg-preview-1.png"
+      className="border rounded-[100%] size-[350px]" />
+    <div>
+      <h1 className="text-3xl font-bold mb-5 text-black">A Very Short Intro About Myself in Google</h1>
+      
+      
+      <p className="py-6 max-w-60 lg:max-w-full">
+      If you search me on <a href="www.google.com" className="underline text-blue-700 font-bold">Google</a> using <b>“Best SEO Expert in Pabna”</b>, You will get my website ranking on the 1st position.You don’t need to trust me, trust Google.
+      </p>
+      
+    </div>
+  </div>
+</div>
+        </section>
+   </div>
+  )
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
+import { About } from "../Components/About"
 import { Introduction } from "../Components/Introduction"
 
 export const Home = () => {
   return (
     <>
        <Introduction></Introduction> 
+       <About></About>
     </>
   )
 }
